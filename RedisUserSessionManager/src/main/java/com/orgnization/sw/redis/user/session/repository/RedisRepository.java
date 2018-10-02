c19f5893e5136bfeda783d31bddb46e8291c8e58
gaurav <gauravsaki@gmail.com>
2018-10-02 23:28:40 +0530

    Added Redis Module

---
+++ RedisUserSessionManager/src/main/java/com/orgnization/sw/redis/user/session/repository/RedisRepository.java
@@ -0,0 +1,30 @@
+package com.orgnization.sw.redis.user.session.repository;
+
+import com.orgnization.sw.redis.user.session.model.UserSession;
+
+import java.util.Map;
+
+
+public interface RedisRepository {
+
+    /**
+     * Return all movies
+     */
+    Map<Object, Object> findAllSessions();
+
+    /**
+     * Add key-value pair to Redis.
+     */
+    void add(UserSession userSession);
+
+    /**
+     * Delete a key-value pair in Redis.
+     */
+    void deleteSession(String id);
+    
+    /**
+     * find a movie
+     */
+    UserSession findSession(String id);
+    
+}
